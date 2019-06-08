ef7c1676ce9e729b28b8678bd49c00f8fab571ad
Hedgestock <arthur.pamart@epita.fr>
2019-06-08 11:17:50 +0200

    Finished "the tools"

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom"
+import * as React from "react"
+import App from "./App"
+
+console.log("Hello World !"); // we'll, it's a classic, isn't it ?
+ReactDOM.render(<App/>, document.getElementById('root'))
